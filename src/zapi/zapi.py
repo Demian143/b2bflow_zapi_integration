fde4082c7e5d37ebb213cd75f8b60fa61ef13fe6
Demian <patrickxavier140@gmail.com>
2026-06-21 15:37:32 -0300

    Envio de mensagens configurado

--- src/zapi/zapi.py
+++ src/zapi/zapi.py
@@ -7,12 +7,18 @@ load_dotenv()
 ZAPI_BASE_URL = os.environ.get("ZAPI_BASE_URL")
 ZAPI_ID = os.environ.get("ZAPI_ID")
 ZAPI_TOKEN = os.environ.get("ZAPI_TOKEN")
+ZAPI_CLIENT_TOKEN = os.environ.get("ZAPI_CLIENT_TOKEN")
 
 class ZAPIConnector:
-    def __init__(self, zapi_base_url: str = ZAPI_BASE_URL, zapi_id: str = ZAPI_ID, zapi_token: str = ZAPI_TOKEN):
+    def __init__(self, 
+                 zapi_base_url: str = ZAPI_BASE_URL, 
+                 zapi_id: str = ZAPI_ID, 
+                 zapi_token: str = ZAPI_TOKEN,
+                 zapi_client_token: str = ZAPI_CLIENT_TOKEN):
         self.zapi_url = zapi_base_url
         self.zapi_id = zapi_id
         self.zapi_token = zapi_token
+        self.zapi_client_token = zapi_client_token
         self.endpoint: str = zapi_base_url + zapi_id + "/token/" + zapi_token
 
     def send_default_message(self, user_name: str, phone_number: str):
@@ -23,8 +29,9 @@ class ZAPIConnector:
                 }
         
         headers = {
-                    "Client-Token": self.zapi_token,
+                    "Client-Token": self.zapi_client_token,
                     "Content-Type": "application/json"
                 }
-        response = requests.post(self.endpoint, json=payload, headers=headers)
+        response = requests.post(self.endpoint + "/send-text", json=payload, headers=headers)
+        print(response.status_code)
         print(response.text)
