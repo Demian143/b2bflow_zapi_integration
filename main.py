3f530311c371e183fa6b0bc02999ab9ec0364d65
Demian <patrickxavier140@gmail.com>
2026-06-21 15:56:38 -0300

    Atualização do nome da classe MessageManager

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
-from src.message_manager.message_manager import MessageMenager
+from src.message_manager.message_manager import MessageManager
 
 app = FastAPI()
 
 @app.get("/send_default_message")
 def send_default_message():
     try:
-        MessageMenager().send_default_messages()
+        MessageManager().send_default_messages()
     except Exception:
         return 500
     return 200
